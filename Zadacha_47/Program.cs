1aae5a922908cb01862e93fd60472c8308771e56
Vadim V P <Everestxxx555@yandex.ru>
2023-01-23 01:05:05 +0300

    коммит Zadacha_47

---
+++ Zadacha_47/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив размером m x n, заполненный
+// случайными вещественными числами.
+// m = 3, n = 4.
+// 0,57 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+void ShowArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}; ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    double[,] array = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = rand.Next(leftRange, rightRange + 1);
+            double r = Math.Round(rand.NextDouble(), 1);
+            array[i,j] += r;
+        }
+    }
+    return array;
+}
+
+int EnterNumber(string message)  //message - сообщение
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// получить от пользователя M и N 
+int m = EnterNumber("введите значение m ");
+int n = EnterNumber("введите значение n ");
+
+// создать двумерный массив и заполнить 
+double[,] matrix = CreateRandomArray(m, n, -10, 10); // Создать случайный массив
+
+// вывести массив в консоль
+ShowArray(matrix); // Показать массив
